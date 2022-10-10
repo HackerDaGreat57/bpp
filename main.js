e7822af6122c34467c6a49cd639cfdd3c6ce4f19
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-10 10:59:41 +0530

    Pulling my hair out

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ const reactElement = React.createElement;
 
 class AppTitle extends React.Component {
   render() {
-    return React.createElement("div", React.createElement("span", {
+    return React.createElement("div", ' ', React.createElement("span", {
       style: {
         fontWeight: 'bold'
       }
-    }), null, "Blender++ Live");
+    }, null, "Blender++ Live"));
   }
 }
 
 const domContainer = document.querySelector('#render_area');
 const root = ReactDOM.createRoot(domContainer);
 root.render(reactElement(AppTitle));
+
+export default function App() {
+  return /*#__PURE__*/React.createElement("div", null, "Coding", ' ', /*#__PURE__*/React.createElement("span", {
+    style: {
+      fontWeight: 'bold'
+    }
+  }, "Beauty"));
+}
