a114bbce7690531d72d22aaaa69795e378968269
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-10 11:55:18 +0530

    Maybe it works now???

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ const reactElement = React.createElement;
 
 class AppTitle extends React.Component {
   render() {
-    return React.createElement("div", ' ', React.createElement("span", "Blender++ Live"));
+    return React.createElement("div", null, React.createElement("span", {
+      className: "apptitle"
+    }, null, "Blender++ Live"));
   }
 }
 
 const domContainer = document.querySelector('#render_area');
 const root = ReactDOM.createRoot(domContainer);
 root.render(reactElement(AppTitle));
+
+export default function App() {
+  return React.createElement("div", null, "Coding ", React.createElement("span", {className: "bold"}, "Beauty"));
+}
