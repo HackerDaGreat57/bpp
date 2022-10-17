4c3661ded1d0dbcf938fe9e6833f31603cd74bc4
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 06:35:08 -0700

    Let's see if MS Paint works

--- test.js
+++ test.js
@@ -1,66 +1,126 @@
-const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const TODOS_LS = "toDos";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: 'black',
+      width: '1',
+      height: '1',
+      x: '0',
+      y: '0',
+      isDrawing: false,
+      isErasing: false,
+      isFilling: false,
+      isMoving: false,
+      isResizing: false,
+      isSelecting: false,
+      isSaving: false,
+      isLoading: false,
+      isClearing: false,
+      isUndoing: false,
+      isRedoing: false,
+      isDeleting: false,
+      isCopying: false,
+      isPasting: false,
+      isCutting: false,
+      isDeselecting: false,
+      isSelectingAll: false,
+      isInverting: false,
+      isRotating: false,
+      isFlipping: false,
+      isMirroring: false,
+      isZooming: false,
+      isCropping: false,
+      isAdjusting: false,
+      isFiltering: false,
+      isBlurring: false,
+      isSharpening: false,
+      isSmoothing: false,
+      isEmbossing: false,
+      isEdgeDetecting: false,
+      isGrayscaling: false,
+      isSepiaToning: false,
+      isPosterizing: false,
+      isSolarizing: false,
+      isThresholding: false,
+      isDithering: false,
+      isInvertingColors: false,
+      isChangingColors: false,
+      isChangingBrightness: false,
+      isChangingContrast: false,
+      isChangingSaturation: false,
+      isChangingHue: false,
+      isChangingGamma: false,
+      isChangingExposure: false,
+      isChangingTemperature: false,
+      isChangingTint: false,
+      isChangingHighlights: false,
+      isChangingShadows: false,
+      isChangingWhites: false,
+      isChangingBlacks: false,
+      isChangingClarity: false,
+      isChangingVibrance: false,
+      isChangingDehaze: false,
+      isChangingNoise: false,
+      isChangingPixelate: false,
+      isChangingBlur: false,
+      isChangingSharpen: false,
+      isChangingEmboss: false,
+      isChangingEdgeDetect: false,
+      isChangingGrayscale: false,
+      isChangingSepia: false,
+      isChangingPosterize: false,
+      isChangingSolarize: false,
+      isChangingThreshold: false,
+      isChangingDither: false,
+      isChangingInvertColors: false,
+      isChangingColorBalance: false,
+      isChangingBrightnessContrast: false,
+      isChangingSaturationLightness: false,
+      isChangingHueSaturation: false,
+      isChangingGammaExposure: false,
+      isChangingTemperatureTint: false,
+      isChangingHighlightsShadows: false,
+      isChangingWhitesBlacks: false,
+      isChangingClarityVibrance: false,
+      isChangingDehazeNoise: false,
+      isChangingPixelateBlur: false,
+      isChangingSharpenEmboss: false,
+      isChangingEdgeDetectGrayscale: false,
+      isChangingSepiaPosterize: false,
+      isChangingSolarizeThreshold: false,
+      isChangingDitherInvertColors: false,
+      isChangingColorBalanceBrightnessContrast: false,
+      isChangingSaturationLightnessHueSaturation: false,
+      isChangingGammaExposureTemperatureTint: false,
+      isChangingHighlightsShadowsWhitesBlacks: false,
+      isChangingClarityVibranceDehazeNoise: false,
+      isChangingPixelateBlurSharpenEmboss: false,
+      isChangingEdgeDetectGrayscaleSepiaPosterize: false,
+      isChangingSolarizeThresholdDitherInvertColors: false,
+      isChangingColorBalanceBrightnessContrastSaturationLightnessHueSaturation: false,
+      isChangingGammaExposureTemperatureTintHighlightsShadowsWhitesBlacks: false,
+      isChangingClarityVibranceDehazeNoisePixelateBlurSharpenEmboss: false,
+      isChangingEdgeDetectGrayscaleSepiaPosterizeSolarizeThresholdDitherInvertColors: false,
+      isChangingColorBalanceBrightnessContrastSaturationLightnessHueSaturationGammaExposureTemperatureTintHighlightsShadowsWhitesBlacks: false,
+      isChangingClarityVibranceDehazeNoisePixelateBlurSharpenEmbossEdgeDetectGrayscaleSepiaPosterizeSolarizeThresholdDitherInvertColors: false,
+      isChangingColorBalanceBrightnessContrastSaturationLightnessHueSaturationGammaExposureTemperatureTintHighlightsShadowsWhitesBlacksClarityVibranceDehazeNoisePixelateBlurSharpenEmbossEdgeDetectGrayscaleSepiaPosterizeSolarizeThresholdDitherInvertColors: false
+    };
+  }
 
-let toDos = [];
-
-function deleteToDo(event) {
-  const btn = event.target;
-  const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function(toDo) {
-    return toDo.id !== parseInt(li.id);
-  });
-  toDos = cleanToDos;
-  saveToDos();
-}
-
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-}
-
-function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", deleteToDo);
-  span.innerText = text;
-  li.appendChild(delBtn);
-  li.appendChild(span);
-  li.id = newId;
-  toDoList.appendChild(li);
-  const toDoObj = {
-    text: text,
-    id: newId
-  };
-  toDos.push(toDoObj);
-  saveToDos();
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
-  toDoInput.value = "";
-}
-
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo) {
-      paintToDo(toDo.text);
-    });
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'h1',
+        null,
+        'Hello World!'
+      )
+    );
   }
 }
 
-function init() {
-  loadToDos();
-  toDoForm.addEventListener("submit", handleSubmit);
-}
-
-init();
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
