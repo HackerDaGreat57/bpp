17d42d1f9c01318788a8c8090e72ea9d04c08fb3
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 01:14:08 -0700

    I wouldn't be alive without the Internet

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 //main.js: Blender++ Live main source code
+
+//Create the screen, basically
 const reactElement = React.createElement;
+
+//Styles
+const appTitle_style = {
+  fontFamily: 'Tahoma',
+  fontSize: '5em',
+  fontWeight: 'bold',
+  color: 'black'
+};
 
 class AppTitle extends React.Component {
   render() {
     return React.createElement("div", null, React.createElement("span", {
-      className: "apptitle"
+      style: appTitle_style
     }, null, "Blender++ Live"));
   }
 }
