05b50b8d4f2378e6c0b7e1f19d4b311f0ed5fb27
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 01:36:27 -0700

    Ok we'll just use 100% inline styles

--- main.js
+++ main.js
@@ -3,14 +3,6 @@
 //Create the screen, basically
 const reactElement = React.createElement;
 
-//Styles
-const appTitle_style = {
-  fontFamily: 'Tahoma',
-  fontSize: '2em',
-  fontWeight: 'bold',
-  color: 'black'
-};
-
 function App() {
   return React.createElement("div", {
     style: {
@@ -25,7 +17,12 @@ export default App;
 class AppTitle extends React.Component {
   render() {
     return React.createElement("div", null, React.createElement("span", {
-      style: appTitle_style
+      style: {
+        fontFamily: 'Tahoma',
+        fontSize: '2em',
+        fontWeight: 'bold',
+        color: 'white'
+      }
     }, null, "Blender++ Live"));
   }
 }
