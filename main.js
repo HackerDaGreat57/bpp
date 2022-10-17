52de135850ac38b4b4fc77fd79872badc16b7bbe
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 03:02:35 -0700

    ALL HAIL AI CODE ASSISTANTS

--- main.js
+++ main.js
@@ -13,23 +13,39 @@ class app_title extends React.Component { //Create the title element.
       }
     }, null, "Blender++ Live");
   }
-}
+};
 
-class start_popup extends React.Component { //Create the pop-up box.
+class start_popup extends React.Component { //Create the start popup.
   render() {
     return React.createElement("div", {
       style: {
+        backgroundColor: 'white',
+        width: '50%',
+        height: '50%',
+        borderRadius: '10px',
+        position: 'absolute',
+        top: '25%',
+        left: '25%',
+        boxShadow: '0px 0px 10px black'
+      }
+    }, null, React.createElement("div", {
+      style: {
         backgroundColor: '#454545',
         width: '100%',
-        height: '100%',
-        position: 'relative',
-        top: '5em',
-        left: '5em',
-        zIndex: '1'
+        height: '10%',
+        borderTopLeftRadius: '10px',
+        borderTopRightRadius: '10px',
+        color: 'white',
+        fontFamily: 'bpp_Tahoma',
+        fontSize: '1.5em',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingTop: '10px'
       }
-    }, null, "Hello World!");
+    }, null, "Welcome to Blender++ Live!"));
   }
 }
+
 
 //Set the background style.
 document.body.style.backgroundColor = '#454545';
