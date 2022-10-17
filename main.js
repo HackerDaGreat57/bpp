c1391a14db41159aed6e4050a6bbcb74b8630997
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 04:39:37 -0700

    Darken shadow

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ class start_popup extends React.Component { //Create the start screen pop-up.
         position: 'absolute',
         top: this.state.y,
         left: this.state.x,
-        boxShadow: '0em 0em 1em #404040'
+        boxShadow: '0em 0em 1em #202020'
       }
     }, null, React.createElement("div", { //Title bar
       style: {
