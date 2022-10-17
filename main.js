cc58d20ffc89c00a4dfeab4a8d5caf6d2db0cad3
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 02:55:49 -0700

    Pop-up box test

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 //main.js: Blender++ Live main source code
 
-//Create the screen, basically
-const reactElement = React.createElement;
+const reactElement = React.createElement; //Create the screen, basically.
 
-class app_title extends React.Component {
+class app_title extends React.Component { //Create the title element.
   render() {
     return React.createElement("span", {
       style: {
@@ -16,10 +15,29 @@ class app_title extends React.Component {
   }
 }
 
+class start_popup extends React.Component { //Create the pop-up box.
+  render() {
+    return React.createElement("div", {
+      style: {
+        backgroundColor: '#454545',
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        zIndex: '1'
+      }
+    }, null, "Hello World!");
+  }
+}
+
+//Set the background style.
 document.body.style.backgroundColor = '#454545';
 document.body.style.width = '100%';
 document.body.style.height = '100%';
 
+//Finish up and render stuff
 const domContainer = document.querySelector('#render_area');
 const root = ReactDOM.createRoot(domContainer);
 root.render(reactElement(app_title));
+root.render(reactElement(start_popup));
