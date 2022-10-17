cdd607d06f232c3ef1cb1bb8fd5e1b0f35fade75
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 02:14:32 -0700

    I'll just do it from plain HTML if this doesn't work

--- main.js
+++ main.js
@@ -3,19 +3,6 @@
 //Create the screen, basically
 const reactElement = React.createElement;
 
-class app_background extends React.Component{
-  render() {
-    return React.createElement("div", {
-      style: {
-        backgroundColor: '#454545',
-        foregroundColor: '#454545',
-        width: '100%',
-        height: '100%'
-      }
-    });
-  }
-}
-
 class app_title extends React.Component {
   render() {
     return React.createElement("div", null, React.createElement("span", {
@@ -29,14 +16,10 @@ class app_title extends React.Component {
   }
 }
 
-document.body.style = {
-  backgroundColor: '#454545',
-  foregroundColor: '#454545',
-  width: '100%',
-  height: '100%'
-}
+document.body.style.backgroundColor = '#454545';
+document.body.style.width = '100%';
+document.body.style.height = '100%';
 
 const domContainer = document.querySelector('#render_area');
 const root = ReactDOM.createRoot(domContainer);
-root.render(reactElement(app_background));
 root.render(reactElement(app_title));
