502d3038f1010afc784fd33c7622456cd878c908
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-17 03:11:06 -0700

    I think my robots have reached their limits

--- main.js
+++ main.js
@@ -15,37 +15,25 @@ class app_title extends React.Component { //Create the title element.
   }
 };
 
-class start_popup extends React.Component { //Create the start popup.
+class start_popup extends React.Component { //Create the start pop-up.
   render() {
     return React.createElement("div", {
       style: {
-        backgroundColor: 'white',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
         width: '50%',
         height: '50%',
+        backgroundColor: 'white',
         borderRadius: '10px',
-        position: 'absolute',
-        top: '25%',
-        left: '25%',
-        boxShadow: '0px 0px 10px black'
+        boxShadow: '0px 0px 10px #000000',
+        animation: 'fly-in 0.5s ease-in-out',
+        animationFillMode: 'forwards'
       }
-    }, null, React.createElement("div", {
-      style: {
-        backgroundColor: '#454545',
-        width: '100%',
-        height: '10%',
-        borderTopLeftRadius: '10px',
-        borderTopRightRadius: '10px',
-        color: 'white',
-        fontFamily: 'bpp_Tahoma',
-        fontSize: '1.5em',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        paddingTop: '10px'
-      }
-    }, null, "Welcome to Blender++ Live!"));
+    }, null, "Welcome to Blender++ Live!");
   }
-}
-
+};
 
 //Set the background style.
 document.body.style.backgroundColor = '#454545';
