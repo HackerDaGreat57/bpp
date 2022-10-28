6905781dd49523b9c3405eec12f17b8a31a065d9
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-27 19:20:47 -0700

    Backup code before experiment

--- src/web/server.js
+++ src/web/server.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 console.log("Creating HTTP server.");
 
 http.createServer(function(request, response) {
-  var raw = fs.createReadStream('index.js');
+  var index_html = fs.createReadStream('index.html');
+  var index_js = fs.createReadStream('index.js');
+  var index_css = fs.createReadStream('index.css');
   var acceptEncoding = request.headers['accept-encoding'];
   if (!acceptEncoding) {
     acceptEncoding = '';
@@ -16,19 +18,27 @@ http.createServer(function(request, response) {
   if (acceptEncoding.match("br")) {
     console.log("br");
 		response.writeHead(200, {'content-encoding': 'brotli'});
-    raw.pipe(zlib.createBrotliCompress()).pipe(response);
+    index_html.pipe(zlib.createBrotliCompress()).pipe(response);
+    index_css.pipe(zlib.createBrotliCompress()).pipe(response);
+    index_js.pipe(zlib.createBrotliCompress()).pipe(response);
   } else if (acceptEncoding.match("gzip")) {
     console.log("gzip");
 		response.writeHead(200, {'content-encoding': 'gzip'});
-    raw.pipe(zlib.createGzip()).pipe(response);
+    index_html.pipe(zlib.createGzip()).pipe(response);
+    index_css.pipe(zlib.createGzip()).pipe(response);
+    index_js.pipe(zlib.createGzip()).pipe(response);
 	} else if (acceptEncoding.match("deflate")) {
     console.log("deflate");
 		response.writeHead(200, {'content-encoding': 'deflate'});
-    raw.pipe(zlib.createDeflate()).pipe(response);
+    index_html.pipe(zlib.createDeflate()).pipe(response);
+    index_css.pipe(zlib.createDeflate()).pipe(response);
+    index_js.pipe(zlib.createDeflate()).pipe(response);
   } else {
     console.log("regular");
     response.writeHead(200, {});
-    raw.pipe(response);
+    index_html.pipe(response);
+    index_css.pipe(response);
+    index_js.pipe(response);
   }
 }).listen(3726);
 console.log("Finished creating HTTP server.");
